435f6ebb5897582422a5bb47f797da8971f8c7fa
root <root@ubuntu-server-01.myguest.virtualbox.org>
2024-08-22 18:45:36 +0900

    remote

---
+++ remote_aspire-starter-first/aspire-starter-first.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.aspire_starter_first_ApiService>("apiservice");
+
+builder.AddProject<Projects.aspire_starter_first_Web>("webfrontend")
+    //.WithHttpsEndpoint(port: 5030)
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
